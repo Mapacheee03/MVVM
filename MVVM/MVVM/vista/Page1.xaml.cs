81b0e34665b433fd1767fe19cc3a89a10ec698d4
Orlxndo <Orlxndo@DESKTOP-ISPFREG>
2023-10-16 11:41:12 -0700

    Agregar archivos de proyecto.

---
+++ MVVM/MVVM/vista/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MVVM.vistaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM.vista
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = new VMpagina1(Navigation);
+        }
+    }
+}
---
+++ MVVM/MVVM/vistaModelo/VMpatron.cs
@@ -0,0 +1,46 @@
+﻿using MVVM.VistaModelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM.vistaModelo
+{
+    public class VMpatron : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+
+        #region Contructor
+        public VMpatron(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+
+        #region Objetivo;
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcecesoAsyncrono()
+        {
+        }
+
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion.
+        #region CONTRUCTOR
+        public ICommand ProcesoAsynCommand => new Command(async () => await ProcecesoAsyncrono());
+        public ICommand ProcesoSimpCommand => new Command(ProcesoSimple);
+        #endregion.
+    }
+}
